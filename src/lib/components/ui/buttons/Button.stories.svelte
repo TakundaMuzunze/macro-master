3b6d80e8a1636bf4d20efdf1af091caf4142f53c
Takunda Muzunze <takmuzunze@gmail.com>
2025-05-26 13:52:15 +0100

    feat: create button component with different variants and default styles

---
+++ src/lib/components/ui/buttons/Button.stories.svelte
@@ -0,0 +1,24 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Button from './Button.svelte';
+
+	const { Story } = defineMeta({
+		component: Button,
+		tags: ['autodocs'],
+		argTypes: {
+			variant: {
+				control: { type: 'select' },
+				options: ['primary', 'secondary', 'ghost']
+			},
+			size: {
+				control: { type: 'select' },
+				options: ['sm', 'md', 'lg']
+			},
+			text: {
+				control: 'text'
+			}
+		}
+	});
+</script>
+
+<Story name="Base Button" />
---
+++ src/lib/components/ui/buttons/Button.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	let { size, variant, event, text } = $props();
+
+	const variants: Record<typeof variant, string> = {
+		primary: 'bg-primary-btn rounded-lg text-primary-text',
+		secondary: 'bg-secondary-btn rounded-lg text-primary-text',
+		ghost: 'bg-transparent rounded-lg border-2 border-primary-btn text-primary-btn'
+	};
+
+	const buttonSizes: Record<typeof size, string> = {
+		sm: 'px-4 py-2',
+		md: 'px-6 py-3',
+		lg: 'px-8 py-4'
+	};
+</script>
+
+<button
+	onclick={event}
+	aria-label={text}
+	class={` shadow-accent/80 inline-flex cursor-pointer items-center gap-2 rounded-2xl font-medium transition-all ease-in-out hover:-translate-y-1 hover:shadow-md
+    ${variants[variant]} ${buttonSizes[size]}`}>{text}</button
+>
