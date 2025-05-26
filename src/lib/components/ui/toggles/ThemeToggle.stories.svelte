d5dbaa3f2cb22bc08532a50d3fc2a2cd334e74f3
Takunda Muzunze <takmuzunze@gmail.com>
2025-05-26 14:51:41 +0100

    feat: create toggle switch to change application theme state

---
+++ src/lib/components/ui/toggles/ThemeToggle.stories.svelte
@@ -0,0 +1,12 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import ThemeToggle from './ThemeToggle.svelte';
+
+	const { Story } = defineMeta({
+		component: ThemeToggle,
+		tags: ['autodocs'],
+		title: 'Toggles/Toggle Switch'
+	});
+</script>
+
+<Story name="Theme Switch" />
