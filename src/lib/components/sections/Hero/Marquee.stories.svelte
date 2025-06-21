9155e79b2220a90c73233e81ed367d0c999d35cc
Takunda Muzunze <takmuzunze@gmail.com>
2025-06-21 23:23:35 +0100

    feat(TM): install svelte fast marquee and create marquee slide with feature cards

---
+++ src/lib/components/sections/Hero/Marquee.stories.svelte
@@ -0,0 +1,12 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Marquee from './Marquee.svelte';
+
+	const { Story } = defineMeta({
+		component: Marquee,
+		tags: ['autodocs'],
+		title: 'Sections/Hero/Marquee'
+	});
+</script>
+
+<Story name="Marquee Slideshow" />
