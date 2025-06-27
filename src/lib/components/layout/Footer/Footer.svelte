82edaa9d3b24d4c1a757a2030c64d52a70998fdc
Takunda Muzunze <takmuzunze@gmail.com>
2025-06-28 00:16:37 +0100

    feat: create footer component

---
+++ src/lib/components/layout/Footer/Footer.svelte
@@ -0,0 +1,9 @@
+<script>
+	const date = new Date().getFullYear();
+</script>
+
+<div
+	class="border-whte text-primary mt-auto flex w-full items-center justify-center border-t-2 p-5 font-normal"
+>
+	&copy; {date} MacroMaster | All rights reserved
+</div>
