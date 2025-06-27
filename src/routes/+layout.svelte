190e7f0f378de93660b7a98597ae2f364ee48500
Takunda Muzunze <takmuzunze@gmail.com>
2025-06-28 00:17:07 +0100

    refactor: remove header footer and header from page.svelte and move to layout file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,24 @@
 <script lang="ts">
+	import Footer from '$lib/components/layout/Footer/Footer.svelte';
+	import Header from '$lib/components/layout/Header/Header.svelte';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
 
-{@render children()}
+<a
+	href="#main-content"
+	class="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-50 focus:rounded focus:bg-blue-600 focus:px-4 focus:py-2 focus:text-white focus:ring-2 focus:ring-blue-500 focus:outline-none"
+>
+	Skip to main content
+</a>
+
+<div class="relative min-h-screen">
+	<Header />
+
+	<main id="main-content" class="flex h-full flex-col pt-[5rem]">
+		{@render children()}
+	</main>
+
+	<Footer />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,19 +4,5 @@
 	import HowItWorks from '$lib/components/sections/HowItWorks/HowItWorks.svelte';
 </script>
 
-<a
-	href="#main-content"
-	class="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-50 focus:rounded focus:bg-blue-600 focus:px-4 focus:py-2 focus:text-white focus:ring-2 focus:ring-blue-500 focus:outline-none"
->
-	Skip to main content
-</a>
-
-<div class="relative min-h-screen">
-	<Header />
-
-	<main id="main-content" class="flex h-full flex-col pt-[5rem]">
-		<Hero />
-	</main>
-
-	<HowItWorks />
-</div>
+<Hero />
+<HowItWorks />
