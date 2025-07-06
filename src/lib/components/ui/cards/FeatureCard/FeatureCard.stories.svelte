e2e40cfc4edc38225206a7bed79d8295fe2bb188
Takunda Muzunze <takmuzunze@gmail.com>
2025-07-06 18:21:01 +0100

    feat: create constants file for features and create card component

---
+++ src/lib/components/ui/cards/FeatureCard/FeatureCard.stories.svelte
@@ -0,0 +1,12 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import FeatureCard from './FeatureCard.svelte';
+
+	const { Story } = defineMeta({
+		component: FeatureCard,
+		tags: ['autodocs'],
+		title: 'Cards/Feature Card'
+	});
+</script>
+
+<Story name="Feature Card Highlight" />
---
+++ src/lib/components/ui/cards/FeatureCard/FeatureCard.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	let { feature } = $props();
+</script>
+
+<article
+	class="bg-surface shadow-accent/30 flex h-full max-w-2xl flex-col items-center justify-center gap-6 rounded-2xl px-5 py-10 text-center transition-all ease-in-out hover:-translate-y-2 hover:shadow-md"
+>
+	<p class="text-primary text-4xl font-light tracking-wide">{feature.icon}</p>
+	<h3 class="text-primary text-xl tracking-wider text-balance md:text-2xl">
+		{feature.title}
+	</h3>
+	<p class="text-primary font-light tracking-wide">{feature.description}</p>
+</article>
