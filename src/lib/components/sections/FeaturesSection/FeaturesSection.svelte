d659d8c074e06e198f7017ded41aa1a3ef02cc22
Takunda Muzunze <takmuzunze@gmail.com>
2025-07-06 18:21:11 +0100

    feat: create section grid for feature cards

---
+++ src/lib/components/sections/FeaturesSection/FeaturesSection.svelte
@@ -0,0 +1,28 @@
+<script>
+	import FeatureCard from '$lib/components/ui/cards/FeatureCard/FeatureCard.svelte';
+	import { featuresHighlights } from '$lib/constants/features';
+</script>
+
+<section
+	class="relative flex w-full flex-col items-center justify-center gap-6 p-10"
+	aria-labelledby="feature-title"
+>
+	<h2 id="feature-title" class="text-primary text-center text-4xl font-normal text-balance">
+		Why MacroMaster is for you
+	</h2>
+	<p class="text-primary text-lg font-light italic">
+		Strong features to assist you in creating dishes and tracking your calories
+	</p>
+
+	<div
+		class="mx-auto grid w-full max-w-4xl grid-cols-2 flex-col gap-5 py-5 lg:py-10"
+		role="list"
+		aria-label="Step-by-step process for using MacroMaster"
+	>
+		{#each featuresHighlights as feature}
+			<div role="listitem">
+				<FeatureCard {feature} />
+			</div>
+		{/each}
+	</div>
+</section>
