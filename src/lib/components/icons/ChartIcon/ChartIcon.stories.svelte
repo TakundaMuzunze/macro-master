b83b8e5d70767dee4008b8c6ae152c750fd1134a
Takunda Muzunze <takmuzunze@gmail.com>
2025-07-06 14:39:37 +0100

    feat: create icon component

---
+++ src/lib/components/icons/ChartIcon/ChartIcon.stories.svelte
@@ -0,0 +1,12 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import ChartIcon from './ChartIcon.svelte';
+
+	const { Story } = defineMeta({
+		component: ChartIcon,
+		tags: ['autodocs'],
+		title: 'Icons/Chart Icon'
+	});
+</script>
+
+<Story name="Graph Icon" />
